273e3bd7b28a5b1c4c8b7771b4c03355d02e2da3
Shuangyin Li <lshuangyin@gmail.com>
2016-12-03 11:24:01 +0800

    update
    update

--- example.sh
+++ example.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#This is simple example how to use CTL for training and testing.
+#This is simple example how to use ratm for training and testing.
 
-#The train set is a very small part of training set with 1,000 documents, and 200 documents are for testing.
+#The train set is a very small part of training set with 3,000 documents, and 600 documents are for testing.
 #
-#Check ../demo/ to show the input files: training set, test set.
-#Check ./output to show the output 
+#Check ../input/ to show the input files: training set, test set, etc.
+#Check ./output to show the output.
 
 make clean
 echo
@@ -19,4 +19,4 @@ time ./ratm est ./input/train setting.txt 10 4 N ./output ./input/init_beta
 
 echo
 
-time ./ctl inf ./demo/test_wiki200.txt setting.txt ./output final ./output
+time ./ratm inf ./input/test setting.txt N ./output/ final ./output
